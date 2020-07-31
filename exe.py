7dee21d3ea5bba2c0919473ed57413a614b8de39
yotunua <myazaki48@au.com>
2020-08-01 03:02:17 +0900

    change message branch system

--- exe.py
+++ exe.py
@@ -14,15 +14,17 @@ client = discord.Client()
 async def on_ready():
     print("Its ok")
 
+
 # receive message
 @client.event
 async def on_message(message):
+    messageArgs = message.content.split()
     # Bot
     if message.author.bot:
         return
 
     # help
-    if message.content == "/help":
+    if messageArgs[0] == "/help":
         await message.channel.send(""
                                    "> **Polacreth**\n"
                                    "> -----------------\n"
@@ -35,11 +37,11 @@ async def on_message(message):
                                    "")
 
     # return date
-    if message.content == "/date":
+    if messageArgs[0] == "/date":
         await message.channel.send(datetime.datetime.now())
 
     # return AtCoder next time
-    if message.content == "/atnext":
+    if messageArgs[0] == "/atnext":
         html = request.urlopen("https://atcoder.jp/contests/?lang=ja")
         time.sleep(1)
         soup = BeautifulSoup(html)
@@ -51,11 +53,11 @@ async def on_message(message):
         timeRequired = tdList[2].text
         rated = tdList[3].text
 
-        await message.channel.send(f"```xml\nTime : {nextTime.split('+')[0]}\nContest : {contestName}\nTime Required : {timeRequired}\nRated : {rated}\n```")
+        await message.channel.send(
+            f"```xml\nTime : {nextTime.split('+')[0]}\nContest : {contestName}\nTime Required : {timeRequired}\nRated : {rated}\n```")
 
     # wiki research sys
-    if message.content.startswith("/wiki"):
-        messageArgs = message.content.split()
+    if messageArgs[0] == "wiki":
         # help for wiki
         if len(messageArgs) == 1:
             await message.channel.send("usage: /wiki [調べたい単語]")
@@ -73,14 +75,15 @@ async def on_message(message):
             page = wikipedia.page(messageArgs[1])
             resultUrl = page.url
             await message.channel.send(f"Results:\n"
-                                   "```\n"
-                                   f"{result}\n"
-                                   f"```\n"
-                                   f"\n"
-                                   f"{resultUrl}")
+                                       "```\n"
+                                       f"{result}\n"
+                                       f"```\n"
+                                       f"\n"
+                                       f"{resultUrl}")
 
         except wikipedia.exceptions.DisambiguationError as e:
             await message.channel.send("キーワードが抽象的であるため、検索結果を絞ることができませんでした。\n"
                                        "キーワードを変更してください。")
+
 
 client.run(secret.TOKEN)
