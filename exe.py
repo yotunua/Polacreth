10f1bf9c89f645939283220cc40a748e4bb637df
yotunua <myazaki48@au.com>
2020-07-31 10:46:26 +0900

    add help and some fix

--- exe.py
+++ exe.py
@@ -3,11 +3,8 @@ import time
 import datetime
 from bs4 import BeautifulSoup
 from urllib import request
-import re
-import token
+import secret
 
-# discord
-TOKEN = token.TOKEN
 client = discord.Client()
 
 
@@ -23,6 +20,18 @@ async def on_message(message):
     # Bot
     if message.author.bot:
         return
+
+    # help
+    if message.content == "/help":
+        await message.channel.send(""
+                                   "> **Polacreth**\n"
+                                   "> -----------------\n"
+                                   "> usage:\n"
+                                   ">   /date ... 現在の日時を表示\n"
+                                   ">   /atnext ... AtCoderの次回コンテスト予定を表示\n"
+                                   "> -----------------\n"
+                                   "> updated on 2020/07/31\n"
+                                   "> by 48clovA\n")
 
     # return date
     if message.content == "/date":
@@ -44,4 +53,4 @@ async def on_message(message):
         await message.channel.send(f"```xml\nTime : {nextTime.split('+')[0]}\nContest : {contestName}\nTime Required : {timeRequired}\nRated : {rated}\n```")
 
 
-client.run(TOKEN)
+client.run(secret.TOKEN)
